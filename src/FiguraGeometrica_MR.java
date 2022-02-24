82831bc1743d3cdb076c33f4d26a6b94d84aebd4
Mykyta Ryasny <MykytaProject@gmail.com>
2022-02-24 17:47:49 +0100

    nuevo método

--- src/FiguraGeometrica_MR.java
+++ src/FiguraGeometrica_MR.java
@@ -19,5 +19,8 @@ public abstract class FiguraGeometrica_MR {
 	public void setTipoFigura(String tipoFigura) {
 		this.tipoFigura = tipoFigura;
 	}
-
+	
+	public double semiPerimetro() {
+		return this.perimetro() / 2;
+	}
 }
