909b462497abf1b50f7ed8bf966fc290e114d432
Mykyta Ryasny <MykytaProject@gmail.com>
2022-02-24 17:50:24 +0100

    versión comentada

--- src/Rectangulo_MR.java
+++ src/Rectangulo_MR.java
@@ -9,11 +9,27 @@ public class Rectangulo_MR extends FiguraGeometrica_MR {
 		l2 = lP;
 	}
 
+	
+	/** 
+	 * @autor Mykyta Ryasny
+	 * @version 1.2
+	 * Multiplica un lado del rectángulo por el otro.
+	 * @return double
+	 * Nos devuelve el área del rectangulo.
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	
+	/** 
+	 * @autor Mykyta Ryasny
+	 * @version 1.2
+	 * Multiplica por dos cada lado y los suma
+	 * @return double
+	 * Nos devuelve el perímetro del rectángulo.
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
